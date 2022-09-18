64405bda17523487cf962bf2455d00c63efd8dd4
Tamada4a <volce.chat@mail.ru>
2022-09-18 21:48:56 +0400

    Created launcher class

---
+++ app/src/main/java/com/example/findyourselfinthephoto/Main.java
@@ -0,0 +1,46 @@
+package com.example.findyourselfinthephoto;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.findyourselfinthephoto.Fragments.FAQ;
+import com.example.findyourselfinthephoto.Fragments.Home;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Main extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @SuppressLint("NonConstantResourceId")
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch(item.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new Home();
+                            break;
+                        case R.id.nav_faq:
+                            selectedFragment = new FAQ();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
+}
