00d2d82676aaa7f4c8d05515788d6ddcfb381bbe
Tamada4a <volce.chat@mail.ru>
2022-09-18 21:47:53 +0400

    Created fragment for FAQ

---
+++ app/src/main/java/com/example/findyourselfinthephoto/Fragments/FAQ.java
@@ -0,0 +1,22 @@
+package com.example.findyourselfinthephoto.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.findyourselfinthephoto.R;
+
+
+public class FAQ extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_faq, container, false);
+    }
+
+}
