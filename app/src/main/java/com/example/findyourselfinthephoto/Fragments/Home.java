be15a172cc6b681fdde6a70a3f57fc7d2785a53f
Tamada4a <volce.chat@mail.ru>
2022-10-01 16:12:41 +0400

    Added button for upload image

--- app/src/main/java/com/example/findyourselfinthephoto/Fragments/Home.java
+++ app/src/main/java/com/example/findyourselfinthephoto/Fragments/Home.java
@@ -1,9 +1,12 @@
 package com.example.findyourselfinthephoto.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -13,10 +16,36 @@ import com.example.findyourselfinthephoto.R;
 
 public class Home extends Fragment {
 
+    private ImageButton upload_button;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        upload_button = (ImageButton)view.findViewById(R.id.upload_button);
+
+        upload_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showFileChooser();
+            }
+        });
+
+        return view;
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        Intent chooser = Intent.createChooser(intent, "Select a file to Upload");
+
+        try {
+            startActivityForResult(chooser, 0);
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(getActivity(), "Install a File Manager!", Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
