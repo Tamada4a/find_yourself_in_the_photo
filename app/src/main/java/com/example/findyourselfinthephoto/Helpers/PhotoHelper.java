2a35a442731383a19724dec28c3e4ef494ac0441
Kirill Simovin <volce.chat@mail.ru>
2022-12-14 01:35:07 +0400

    Added extractFaces and resizeImage methods

--- app/src/main/java/com/example/findyourselfinthephoto/Helpers/PhotoHelper.java
+++ app/src/main/java/com/example/findyourselfinthephoto/Helpers/PhotoHelper.java
@@ -14,6 +14,7 @@ import com.example.findyourselfinthephoto.R;
 
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
 import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.objdetect.CascadeClassifier;
@@ -22,6 +23,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 /*
 * Класс, отвечающий за всякие проверки с фотографиями
@@ -30,7 +32,7 @@ public class PhotoHelper {
 
     private final String TAG = "GetRecognizedFaceUrl";
 
-    private CascadeClassifier cascadeClassifier;
+    private CascadeClassifier _cascadeClassifier;
 
 
     public PhotoHelper(Activity homeActivity){
@@ -38,18 +40,67 @@ public class PhotoHelper {
     }
 
 
+    public PhotoHelper(CascadeClassifier cascadeClassifier){
+        _cascadeClassifier = cascadeClassifier;
+    }
+
+
     public boolean isContainsFace(String path){
         Mat matImage = Imgcodecs.imread(path);
 
-        Mat resizeimage = new Mat();
-        Size scaleSize = new Size(450,450);
-
-        resize(matImage, resizeimage, scaleSize , 0, 0, INTER_AREA);
+        Mat resizedImage = resizeImage(matImage, 2500.0, 2500.0);
 
         MatOfRect faceDetections = new MatOfRect();
-        cascadeClassifier.detectMultiScale(resizeimage, faceDetections);
+        _cascadeClassifier.detectMultiScale(resizedImage, faceDetections);
 
         return !faceDetections.empty();
+    }
+
+
+    @NonNull
+    public ArrayList<Mat> extractFaces(Mat image) {
+        ArrayList<Mat> detectedFace = new ArrayList<Mat>();
+
+        Mat img = resizeImage(image, 2500.0, 2500.0);
+
+        MatOfRect faceDetections = new MatOfRect();
+        _cascadeClassifier.detectMultiScale(img, faceDetections);
+
+        if (!faceDetections.empty()) {
+            for (Rect rect : faceDetections.toArray()) {
+                Mat faceImage = img.submat(rect);
+                detectedFace.add(faceImage);
+            }
+        }
+        return detectedFace;
+    }
+
+
+    public Mat resizeImage(@NonNull Mat inputImg, double width, double height){
+        Mat newImg = inputImg;
+
+        if(inputImg.size().height > height || inputImg.size().width > width){
+
+            double max = 0;
+
+            double inputHeight = inputImg.size().height;
+            double inputWidth = inputImg.size().width;
+
+            if(inputHeight > inputWidth)
+                max = inputHeight;
+            else
+                max = inputWidth;
+
+            double k = max / width;
+
+            double newHeight = inputHeight / k;
+            double newWidth = inputWidth / k;
+
+            Size scaleSize = new Size(newWidth, newHeight);
+
+            resize(inputImg, newImg, scaleSize , 0, 0, INTER_AREA);
+        }
+        return newImg;
     }
 
 
@@ -71,10 +122,10 @@ public class PhotoHelper {
             is.close();
             os.close();
 
-            cascadeClassifier = new CascadeClassifier(mCascadeFile.getAbsolutePath());
-            if (cascadeClassifier.empty()) {
+            _cascadeClassifier = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+            if (_cascadeClassifier.empty()) {
                 Log.e(TAG, "Failed to load cascade classifier");
-                cascadeClassifier = null;
+                _cascadeClassifier = null;
             } else
                 Log.i(TAG, "Loaded cascade classifier from " + mCascadeFile.getAbsolutePath());
 
